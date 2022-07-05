84a71d3e046c2696407d52bf014e58ca84731e81
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2022-07-05 14:24:01 +0900

    es-lintの不具合修正

--- task/javascript-lint.js
+++ task/javascript-lint.js
@@ -6,12 +6,10 @@ import eslint from 'gulp-eslint'; // javascriptコードチェック
 // task
 //////////////////////////////////////////////////////////////////////
 const javascriptLint = done => {
-  gulp.src([path.js.src,
+  return gulp.src([path.js.src,
           '!node_modules/**'])
     .pipe(eslint({ useEslintrc: true })) // .eslintrc を参照
     .pipe(eslint.format());
-
-  done();
 };
 
 export default javascriptLint;
